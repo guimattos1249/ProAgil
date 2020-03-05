599b3c7192b163fd04d059031ba0777311ab0aa7
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-03-05 18:45:43 -0300

    Added Async Await

--- ProAgil.API/Controllers/EventoController.cs
+++ ProAgil.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ProAgil.API.Data;
 using ProAgil.API.Model;
@@ -22,11 +23,12 @@ namespace ProAgil.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
             try
             {
-                var results = _context.Eventos.ToList();
+                var results = await _context.Eventos.ToListAsync();
+
                 return Ok(results);
             }
             catch (System.Exception)
@@ -37,9 +39,18 @@ namespace ProAgil.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Evento> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
         }
     }
 }
