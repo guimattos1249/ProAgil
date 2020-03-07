dd4c9240ee0ed86b2d3a12cd63808bba1456fa54
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-03-07 10:24:12 -0300

    Added two way data binding on filtro and show img

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,10 @@ import { HttpClient } from '@angular/common/http';
 export class EventosComponent implements OnInit {
 
   eventos: any = [];
+  imgWidth = 50;
+  imgMargin = 2;
+  showImg = false;
+  filterList = '';
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +20,9 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alternarImg() {
+    this.showImg = !this.showImg;
+  }
   getEventos() {
     this.http.get('http://localhost:5000/evento').subscribe(
       response => {
